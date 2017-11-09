1b685ac2f49039b51405c35411325473325cfa3d
Michael Lucido <mjl329@cornell.edu>
2017-11-08 23:11:12 -0500

    Switch to RequestAnimationFrame

--- scripts/snow.js
+++ scripts/snow.js
@@ -2,20 +2,19 @@
 
 window.onload = function(){
 	//canvas init
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
+	const canvas = document.getElementById("canvas");
+	const ctx = canvas.getContext("2d");
 
 	//canvas dimensions
-	var W = window.innerWidth;
-	var H = window.innerHeight;
+	const W = window.innerWidth;
+	const H = window.innerHeight;
 	canvas.width = W;
 	canvas.height = H;
 
 	//snowflake particles
-	var mp = 25; //max particles
-	var particles = [];
-	for(var i = 0; i < mp; i++)
-	{
+	const mp = 25; //max particles
+	const particles = [];
+	for(let i = 0; i < mp; i++){
 		particles.push({
 			x: Math.random()*W, //x-coordinate
 			y: Math.random()*H, //y-coordinate
@@ -24,57 +23,56 @@ window.onload = function(){
 		})
 	}
 
+	let cur; 
+
 	//Lets draw the flakes
-	function draw()
-	{
+	function draw(timestamp){
+		if(!cur) cur = timestamp 
+		const dt = timestamp - cur 
+		cur = timestamp 
+
 		ctx.clearRect(0, 0, W, H);
 
 		ctx.fillStyle = "rgba(255, 255, 255, 1)";
 		ctx.beginPath();
-		for(var i = 0; i < mp; i++)
-		{
-			var p = particles[i];
+		for(let i = 0; i < mp; i++){
+			const p = particles[i];
 			ctx.moveTo(p.x, p.y);
 			ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
 		}
 		ctx.fill();
-		update();
+		update(dt);
+		requestAnimationFrame(draw)
 	}
 
 	//Function to move the snowflakes
 	//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
-	var angle = 0;
-	function update()
-	{
-		angle += 0.01;
-		for(var i = 0; i < mp; i++)
-		{
-			var p = particles[i];
+	let angle = 0;
+	function update(dt){
+		const af = dt / 100; 
+		const pf = dt / 50; 
+		angle += 0.01 * af; 
+
+		for(let i = 0; i < mp; i++){
+			let p = particles[i];
 			//Updating X and Y coordinates
 			//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
 			//Every particle has its own density which can be used to make the downward movement different for each flake
 			//Lets make it more random by adding in the radius
-			p.y += Math.cos(angle+p.d) + 1 + p.r/2;
-			p.x += Math.sin(angle) * 2;
+			p.y += Math.cos(angle+p.d) + 1 + p.r/2 * pf ;
+			p.x += Math.sin(angle) * 2 * pf;
 
 			//Sending flakes back from the top when it exits
 			//Lets make it a bit more organic and let flakes enter from the left and right also.
-			if(p.x > W+5 || p.x < -5 || p.y > H)
-			{
-				if(i%3 > 0) //66.67% of the flakes
-				{
+			if(p.x > W+5 || p.x < -5 || p.y > H){
+				if(i%3 > 0){ //66.67% of the flakes
 					particles[i] = {x: Math.random()*W, y: -10, r: p.r, d: p.d};
-				}
-				else
-				{
+				}else{
 					//If the flake is exitting from the right
-					if(Math.sin(angle) > 0)
-					{
+					if(Math.sin(angle) > 0){
 						//Enter from the left
 						particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
-					}
-					else
-					{
+					}else{
 						//Enter from the right
 						particles[i] = {x: W+5, y: Math.random()*H, r: p.r, d: p.d};
 					}
@@ -84,5 +82,5 @@ window.onload = function(){
 	}
 
 	//animation loop
-	setInterval(draw, 33);
+	requestAnimationFrame(draw)
 }
